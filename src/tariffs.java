b2b38f767bd83f83e27d86321fde53dfbb6c06c9
unknown <aleksey_nikeshin@mail.ru>
2020-10-07 23:10:58 +0300

    Tele2Tariffs

---
+++ src/tariffs.java
@@ -0,0 +1,9 @@
+public class tariffs {
+    String tariffName;
+    double numberOfMinutes;
+    String unlimitedForTele2;
+    double internetTraffic;
+    String unlimitedVk;
+    int price;
+    String buyUrl;
+}
